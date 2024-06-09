9366521eab307b70def06c079a9a79f60e6785a1
Abdul Mohiz <skylark7768@gmail.com>
2024-06-09 20:40:14 +0500

    Designed and Implemented whole database in SQL Server. Also Completed Staff Login page.

--- HotelManagementSystem/Form1.Designer.cs
+++ HotelManagementSystem/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HotelManagementSystem
 {
-    partial class Form1
+    partial class loginpage
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginpage));
+            this.idinput = new System.Windows.Forms.TextBox();
+            this.passwordinput = new System.Windows.Forms.TextBox();
+            this.loginbutton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // idinput
+            // 
+            this.idinput.BackColor = System.Drawing.Color.DimGray;
+            this.idinput.ForeColor = System.Drawing.Color.White;
+            this.idinput.Location = new System.Drawing.Point(810, 375);
+            this.idinput.Multiline = true;
+            this.idinput.Name = "idinput";
+            this.idinput.Size = new System.Drawing.Size(307, 29);
+            this.idinput.TabIndex = 0;
+            // 
+            // passwordinput
+            // 
+            this.passwordinput.BackColor = System.Drawing.Color.DimGray;
+            this.passwordinput.ForeColor = System.Drawing.Color.White;
+            this.passwordinput.Location = new System.Drawing.Point(810, 456);
+            this.passwordinput.Multiline = true;
+            this.passwordinput.Name = "passwordinput";
+            this.passwordinput.Size = new System.Drawing.Size(307, 29);
+            this.passwordinput.TabIndex = 1;
+            // 
+            // loginbutton
+            // 
+            this.loginbutton.BackColor = System.Drawing.Color.Transparent;
+            this.loginbutton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("loginbutton.BackgroundImage")));
+            this.loginbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.loginbutton.Location = new System.Drawing.Point(874, 519);
+            this.loginbutton.Name = "loginbutton";
+            this.loginbutton.Size = new System.Drawing.Size(178, 39);
+            this.loginbutton.TabIndex = 2;
+            this.loginbutton.UseVisualStyleBackColor = false;
+            this.loginbutton.Click += new System.EventHandler(this.loginbutton_Click);
+            // 
+            // loginpage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1206, 568);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1262, 749);
+            this.Controls.Add(this.loginbutton);
+            this.Controls.Add(this.passwordinput);
+            this.Controls.Add(this.idinput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "loginpage";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.loginpage_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox idinput;
+        private System.Windows.Forms.TextBox passwordinput;
+        private System.Windows.Forms.Button loginbutton;
     }
 }
 
--- HotelManagementSystem/Form1.cs
+++ HotelManagementSystem/Form1.cs
@@ -2,19 +2,76 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace HotelManagementSystem
 {
-    public partial class Form1 : Form
+
+    public partial class loginpage : Form
     {
-        public Form1()
+        string strCon;
+        SqlConnection conn;
+        int index = 0;
+
+        public loginpage()
         {
             InitializeComponent();
+            strCon = Properties.Settings.Default.HotelDatabase;
+            conn = new SqlConnection(strCon);
+
+        }
+
+        private void loginpage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                string query = "SELECT * FROM Staff";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                bool check = false; 
+
+                for (int i = 0; i < 10; i++)
+                {
+                    DataRow row = table.Rows[i];
+                    if (idinput.Text == row["StaffId"].ToString() && passwordinput.Text == row["Password"].ToString())
+                    {
+                        // Move to the next page
+                        MessageBox.Show("Success");
+                        check = true; // Set check to true if a match is found
+                        break; // Exit the loop once a match is found
+                    }
+                }
+
+                if (!check) // If no match was found after checking all rows
+                {
+                    MessageBox.Show("No such record found. Please verify your entered details.");
+                }
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+
         }
     }
 }
